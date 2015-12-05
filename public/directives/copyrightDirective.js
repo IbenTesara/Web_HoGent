907b78e3cbe68c4be67b616f74e24a6b39aac772
Iben Van de Veire <iben.vandeveire.r1234@student.hogent.be>
2015-12-05 07:47:27 +0100

    Directive ( fail )

--- public/directives/copyrightDirective.js
+++ public/directives/copyrightDirective.js
@@ -1,8 +1,11 @@
-app.directive('ng-copyright', function() {
+app.directive('copyright', function() {
     var directive = {};
 
-    directive.restrict = 'E'; /* restrict this directive to elements */
-    directive.templateUrl = "<span>&copy; 2015 The Hollow Heart</span";
+    directive.restrict = 'E'; 
+    directive.templateUrl = "<span>&copy; {{copyright.info}}</span";
+    directive.scope={
+    	copyright: "info"
+    }
 
     return directive;
 });
